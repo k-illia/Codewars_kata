6b067e3c0db919e9cb243b25e6fd370ca33238fd
илья <illia.kurishko@gmail.com>
2022-08-13 13:52:02 +0300

    oddoreven

---
+++ resolved 7 kyu.js
@@ -0,0 +1,16 @@
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+//
+//     Give your answer as a string matching "odd" or "even".
+//
+//     If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+    let sum = 0
+
+    for (let i = 0; i < array.length; i++) {
+
+        sum = sum + array[i]
+    }
+    let result = (sum % 2 == 0) ? "even" : "odd"
+    return result
+}
