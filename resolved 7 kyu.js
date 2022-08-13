d2d71b642cfe08021151033047396614c16682d1
илья <illia.kurishko@gmail.com>
2022-08-13 14:46:49 +0300

    XO

--- resolved 7 kyu.js
+++ resolved 7 kyu.js
@@ -3,14 +3,42 @@
 //     Give your answer as a string matching "odd" or "even".
 //
 //     If the input array is empty consider it as: [0] (array with a zero).
+//
+// function oddOrEven(array) {
+//     let sum = 0
+//
+//     for (let i = 0; i < array.length; i++) {
+//
+//         sum = sum + array[i]
+//     }
+//     let result = (sum % 2 == 0) ? "even" : "odd"
+//     return result
+// }
+//===========================================================================================================================================================================
+// Проверьте, содержит ли строка одинаковое количество «x» и «o». Метод должен возвращать логическое значение и не учитывать регистр. Строка может содержать любой символ.
+//
+//     Примеры ввода/вывода:
+//
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
 
-function oddOrEven(array) {
-    let sum = 0
-
-    for (let i = 0; i < array.length; i++) {
-
-        sum = sum + array[i]
+function XO(str) {
+    let a = str.toLowerCase()
+    let counter = 0;
+    let signa = 0
+    for (let elem of a) {
+        if (elem == "x") {
+            counter++
+        }
     }
-    let result = (sum % 2 == 0) ? "even" : "odd"
-    return result
+    for (let elem of a) {
+        if (elem == "o") {
+            signa++
+        }
+    }
+    return  counter == signa
 }
+//===========================================================================================================================================================================
