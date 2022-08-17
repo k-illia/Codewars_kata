10099088d792fa345bf050fd0a895dcead4e7f94
илья <illia.kurishko@gmail.com>
2022-08-17 10:56:55 +0300

    toBinary

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-// function isDivideBy(number, a, b) {
-//     if((number / a)%2 === 0 && (number / b)%2 === 0  ) {return true } else {return false}
-//         }
-//
-// console.log(isDivideBy(12, 2, 4))
+
 
--- resolved 8 kyu.js
+++ resolved 8 kyu.js
@@ -63,9 +63,19 @@
 //     return "hello world!"
 // }
 
-function capitalizeWord(word) {
-    let b = word[0].toUpperCase() + word.slice(1) ;
-    return b;
-}
-
-console.log(capitalizeWord("hello"))
+// function capitalizeWord(word) {
+//     let b = word[0].toUpperCase() + word.slice(1) ;
+//     return b;
+// }
+//
+// console.log(capitalizeWord("hello"))
+// =======================================================================================================================================
+// Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+//
+// to_binary(1)  /* should return 1 */
+// to_binary(5)  /* should return 101 */
+// to_binary(11) /* should return 1011 */
+//
+// function toBinary(n){
+//     return +(n).toString(2)
+// }
