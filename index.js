4826ba8a509f15f624fbe29abac0cdfb56ea1036
илья <illia.kurishko@gmail.com>
2022-08-17 14:10:17 +0300

    solution

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// function fakeBin(x){
-// let stingX = x.toString()
-//     let fake = []
-//     for (let i = 0; i < stingX.length; i++) {
-//         if (stingX[i] <= 5) { fake.push(0)} else {fake.push(1)}
-//     }
-//     return fake.join("")
-// }
 //
-// console.log(fakeBin(366058562030849490134388085))
-
--- resolved 7 kyu.js
+++ resolved 7 kyu.js
@@ -131,3 +131,26 @@
 //     return Array.from(new Set(s1 + s2)).sort().join('');
 // }
 // //===========================================================================================================================================================================
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//
+// Examples:
+//
+//     solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+//
+// function solution(str, ending){
+//     console.log(str.slice(3))
+//     let leng = ending.length
+//     if (str.slice(-leng) === ending) {return true}
+//     else if (ending === "") {return true}
+//     else {return false}
+// }
+//
+//
+// function solution(str, ending){
+//     return str.endsWith(ending);
+// }
+//
+//
+// console.log(solution('abc', ''))
+// //===========================================================================================================================================================================
