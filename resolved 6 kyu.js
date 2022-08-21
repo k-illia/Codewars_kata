044fceb0d79d7f5f3284ef8dfce4413de72ae4e2
илья <illia.kurishko@gmail.com>
2022-08-21 14:55:52 +0300

    likes

--- resolved 6 kyu.js
+++ resolved 6 kyu.js
@@ -65,3 +65,36 @@
 //         n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
 // }
 //================================================================================================
+// // Вы, наверное, знаете систему «лайков» из Facebook и
+// // других страниц. Люди могут «лайкать» сообщения в блогах, изображения или другие элементы.
+// //     Мы хотим создать текст, который должен отображаться рядом с таким элементом.
+// //
+// //     Реализуйте функцию, которая принимает массив, содержащий имена людей, которым понравился элемент.
+// //     Он должен возвращать отображаемый текст, как показано в примерах:
+// //
+// //     []                                -->  "no one likes this"
+// //         ["Peter"]                         -->  "Peter likes this"
+// //         ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// //         ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// //         ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// //
+// // Примечание. Для 4 и более имен число "and 2 others"просто увеличивается.
+//
+// function likes(names) {
+//     if (names.length === 0) {
+//         return "no one likes this"
+//     }
+//     for (let i = 0; i < names.length; i++) {
+//         if (names.length === 1) {
+//             return names[0] + " like this"
+//         } else if (names.length === 2) {
+//             return names[0] + " and " + names[1] + " like this"
+//         } else if (names.length === 3) {
+//             return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+//         }
+//         else { return names[0] + ", " + names[1] + " and " +(names.length - 2)+ " others like this"
+//         }
+//     }
+// }
+// console.log(likes(["Alex", "Jacob", "Mark", "Mark", "Mark", "Mark", "Mark", "Mark", "Mark", "Mark", "Max"]  ))
+//================================================================================================
