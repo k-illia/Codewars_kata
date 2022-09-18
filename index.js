a980b5abbda5f2ea7e5ab641754842495113141f
k-illia <illia.kurishko@gmail.com>
2022-09-18 13:40:25 +0300

    confirmEndingNew

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
+function confirmEnding(str, target) {
+ let b = target.toString()
+    console.log( /$/.test(str) )
+}
 
-//
-// function qwe (a) {
-//     return a = 20
-// }
-// qwe(23)
-
-// function confirmEnding(str, target) {
-//
-//     return str.split(' ');
-// }
-//
-// confirmEnding("Bastian", "n");
-// let str1 = "Mary had a little lamb";
-// console.log( /^Mary/.test(str1) ); // true
+confirmEnding("Bastian", "n");
+let str1 = "Mary had a little lamb";
+console.log( /^Mary/.test(str1) ); // true
