22b662a4f56a6420c27f3041ea370da9f4117882
k-illia <illia.kurishko@gmail.com>
2022-09-18 14:55:40 +0300

    repeatStringNumTimes

--- index.js
+++
@@ -1,8 +0,0 @@
-function confirmEnding(str, target) {
- let b = target.toString()
-    console.log( /$/.test(str) )
-}
-
-confirmEnding("Bastian", "n");
-let str1 = "Mary had a little lamb";
-console.log( /^Mary/.test(str1) ); // true
