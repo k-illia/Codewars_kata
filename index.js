6a9520865458f4c498c0f81cc2f555975a383c4c
k-illia <illia.kurishko@gmail.com>
2022-09-18 13:22:55 +0300

    largestOfFour

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//
+// function qwe (a) {
+//     return a = 20
+// }
+// qwe(23)
+
+// function confirmEnding(str, target) {
+//
+//     return str.split(' ');
+// }
+//
+// confirmEnding("Bastian", "n");
+// let str1 = "Mary had a little lamb";
+// console.log( /^Mary/.test(str1) ); // true
